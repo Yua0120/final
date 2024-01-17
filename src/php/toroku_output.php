c4b7496c668bf6f51c00fef191fa716954891410
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-01-17 11:44:32 +0900

    Update toroku_output.php

--- src/php/toroku_output.php
+++ src/php/toroku_output.php
@@ -1,37 +1,32 @@
 <?php
-session_start();
-require 'connect.php';
-try{
-    if(isset($_POST['username']) && isset($_POST['password'])){
-        $pdo = new PDO($connect, USER, PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    session_start();
+    require 'connect.php';
+    try{
+        if(isset($_POST['username']) && isset($_POST['password'])){
+            $pdo = new PDO($connect, USER, PASS);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-        $sql = $pdo->prepare('select user_id from Users where password = ?');
-        $sql->execute([$hashedPassword]);
-        $result = $sql->fetch(PDO::FETCH_ASSOC);
-
-        if(isset($result['user_id'])){
-            header('Location: toroku.php?flag=known');
-
-        }else{
-            $sql = $pdo->prepare('insert into Users (password) values (?)');
-            $sql->execute([$hashedPassword]);
+            $sql = $pdo->prepare('select user_id from Users where user_id = ? AND password = ?');
+            $sql->execute([$_POST['email'],$hashedPassword]);
             $result = $sql->fetch(PDO::FETCH_ASSOC);
 
-            header('Location: index.php?flag=reg');
+            if(isset($result['user_id'])){
+                header('Location: toroku.php?flag=known');
 
-            $_SESSION['User'] = [
-                'id' => $result['user_id']
-            ];
-            // 登録が成功した場合、Top.php にリダイレクト
-            header('Location: index.php');
+            }else{
+                $sql = $pdo->prepare('insert into Users (user_id,password) values (?,?)');
+                $sql->execute([$_POST['email'],$hashedPassword]);
+                $result = $sql->fetch(PDO::FETCH_ASSOC);
+                $_SESSION['User'] = [
+                    'id' => $result['user_id']
+                ];
+                // 登録が成功した場合、Top.php にリダイレクト
+                header('Location: index.php?flag=reg');
+            }
         }
-
-        
+    }catch (PDOException $e){
+        echo '<script>alert("データベースエラー")</script>' . htmlspecialchars($e->getMessage());
+        echo '<a href="toroku.php">新規登録画面に戻る</a><br>';
     }
-}catch (PDOException $e){
-    echo '<script>alert("データベースエラー")</script>' . htmlspecialchars($e->getMessage());
-    echo '<a href="toroku.php">新規登録画面に戻る</a><br>';
-}
 ?>
