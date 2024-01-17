c628b237563ab1d2a2baf120a40a04b1bea535ed
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-01-17 10:52:23 +0900

    Update login.php

--- src/php/login.php
+++ src/php/login.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<body>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン</title>
 </head>
+<body>
     <h1>ログイン</h1>
     <form action="./login_output.php" method="post">
         <p>メールアドレス</p>
