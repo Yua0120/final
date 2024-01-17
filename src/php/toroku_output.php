c3c561b2a0142912cf157fdb0c9a7997fd49a5ce
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-01-17 11:48:58 +0900

    Update toroku_output.php

--- src/php/toroku_output.php
+++ src/php/toroku_output.php
@@ -2,18 +2,18 @@
     session_start();
     require 'connect.php';
     try{
+        var_dump('デバッグ中1');
         if(isset($_POST['username']) && isset($_POST['password'])){
             $pdo = new PDO($connect, USER, PASS);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
+            var_dump('デバッグ中2');
             $sql = $pdo->prepare('select user_id from Users where user_id = ? AND password = ?');
             $sql->execute([$_POST['email'],$hashedPassword]);
             $result = $sql->fetch(PDO::FETCH_ASSOC);
-
+            var_dump('デバッグ中3');
             if(isset($result['user_id'])){
                 header('Location: toroku.php?flag=known');
-
             }else{
                 $sql = $pdo->prepare('insert into Users (user_id,password) values (?,?)');
                 $sql->execute([$_POST['email'],$hashedPassword]);
@@ -21,6 +21,7 @@
                 $_SESSION['User'] = [
                     'id' => $result['user_id']
                 ];
+                var_dump('デバッグ中4');
                 // 登録が成功した場合、Top.php にリダイレクト
                 header('Location: index.php?flag=reg');
             }
