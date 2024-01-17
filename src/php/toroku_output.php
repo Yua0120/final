780fc1ba3386b385afcdb9650fcd87c08a436417
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-01-17 11:58:41 +0900

    Update toroku_output.php

--- src/php/toroku_output.php
+++ src/php/toroku_output.php
@@ -2,26 +2,25 @@
     session_start();
     require 'connect.php';
     try{
-        var_dump('デバッグ中1');
         if(isset($_POST['email']) && isset($_POST['password'])){
             $pdo = new PDO($connect, USER, PASS);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            var_dump('デバッグ中2');
             $sql = $pdo->prepare('select user_id from Users where user_id = ? AND password = ?');
             $sql->execute([$_POST['email'],$hashedPassword]);
             $result = $sql->fetch(PDO::FETCH_ASSOC);
-            var_dump('デバッグ中3');
             if(isset($result['user_id'])){
                 header('Location: toroku.php?flag=known');
             }else{
                 $sql = $pdo->prepare('insert into Users (user_id,password) values (?,?)');
                 $sql->execute([$_POST['email'],$hashedPassword]);
-                $result = $sql->fetch(PDO::FETCH_ASSOC);
+                //ユーザーIDの取得
+                $selectSql = $pdo->prepare('SELECT user_id FROM Users WHERE user_id = ? AND password = ?');
+                $selectSql->execute([$_POST['email'], $hashedPassword]);
+                $result = $selectSql->fetch(PDO::FETCH_ASSOC);
                 $_SESSION['User'] = [
                     'id' => $result['user_id']
                 ];
-                var_dump('デバッグ中4');
                 // 登録が成功した場合、Top.php にリダイレクト
                 header('Location: index.php?flag=reg');
             }
